3f6d1bff5ce040872e3c843064f67502aeba3129
d0lim <limdongyoung0@gmail.com>
2025-07-11 01:03:46 +0900

    init: repo

---
+++ src/main/java/net/effize/bandlog/BandlogBeApplication.java
@@ -0,0 +1,13 @@
+package net.effize.bandlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandlogBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BandlogBeApplication.class, args);
+    }
+
+}
