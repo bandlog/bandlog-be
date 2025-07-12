0f7a4b0aa62bdf1eb3e979dc25d9ddf5884cb570
d0lim <limdongyoung0@gmail.com>
2025-07-12 15:43:45 +0900

    feat: define simple tests for Email

---
+++ bandlog-api/src/test/java/net/effize/bandlog/domain/user/model/EmailTest.java
@@ -0,0 +1,31 @@
+package net.effize.bandlog.domain.user.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class EmailTest {
+    @Test
+    public void 올바른_이메일은_정상적으로_생성되어야_한다() {
+        // arrange
+        String input = "test@example.com";
+
+        // act
+        Email email = Email.of(input);
+
+        // assert
+        assertThat(email).isNotNull();
+    }
+
+    @Test
+    public void 포맷을_만족하지_않는_이메일은_생성_실패해야_한다() {
+        // arrange
+        String input = "test";
+
+        // act & assert
+        assertThatThrownBy(() -> {
+            Email email = Email.of(input);
+        }).isInstanceOf(RuntimeException.class);
+    }
+}
