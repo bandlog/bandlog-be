48ff552b8bb137aeaef388e52bf7af6121b2c99a
d0lim <limdongyoung0@gmail.com>
2025-07-12 15:54:51 +0900

    feat: implement the Email which satisfies tests

--- bandlog-api/src/main/java/net/effize/bandlog/domain/user/model/Email.java
+++ bandlog-api/src/main/java/net/effize/bandlog/domain/user/model/Email.java
@@ -1,7 +1,26 @@
 package net.effize.bandlog.domain.user.model;
 
 public class Email {
+    private Email(String value) {
+        if (!isValid(value)) {
+            throw new RuntimeException("Invalid email address");
+        }
+        this.value = value;
+    }
 
+    public static Email of(String value) {
+        return new Email(value);
+    }
 
-    private String value;
+    private final String value;
+
+    public String fullEmail() {
+        return value;
+    }
+
+    private boolean isValid(String email) {
+        return email != null &&
+                email.contains("@") &&
+                email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$");
+    }
 }
