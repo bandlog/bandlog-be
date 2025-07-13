445976ae899d16a5eeeffaf80430862ae0045138
d0lim <limdongyoung0@gmail.com>
2025-07-13 19:02:50 +0900

    feat: implement a basic Password construction method

--- bandlog-api/src/main/java/net/effize/bandlog/domain/user/model/Password.java
+++ bandlog-api/src/main/java/net/effize/bandlog/domain/user/model/Password.java
@@ -2,4 +2,22 @@ package net.effize.bandlog.domain.user.model;
 
 public class Password {
     private String value;
+
+    private Password(String value) {
+        if (!isValid(value)) {
+            throw new RuntimeException("Invalid password");
+        }
+        this.value = value;
+    }
+
+    public static Password of(String value) {
+        return new Password(value);
+    }
+
+    private boolean isValid(String password) {
+        return password.length() >= 8 &&
+                password.matches(
+                        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).{8,}$"
+                );
+    }
 }
