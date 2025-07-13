44b22a3d3207a3364d444c59e6f1de1a5f34a3ce
d0lim <limdongyoung0@gmail.com>
2025-07-13 19:34:40 +0900

    feat: add BandlogException constructor without cause

--- bandlog-api/src/main/java/net/effize/bandlog/domain/common/exception/BandlogException.java
+++ bandlog-api/src/main/java/net/effize/bandlog/domain/common/exception/BandlogException.java
@@ -6,6 +6,13 @@ public class BandlogException extends RuntimeException {
     private final String logMessage;
     private final Integer httpStatusCode;
 
+    public BandlogException(ErrorCode errorCode, String userMessage, String logMessage, Integer httpStatusCode) {
+        super(logMessage);
+        this.errorCode = errorCode;
+        this.userMessage = userMessage;
+        this.logMessage = logMessage;
+        this.httpStatusCode = httpStatusCode;
+    }
 
     public BandlogException(ErrorCode errorCode, String userMessage, String logMessage, Integer httpStatusCode, Throwable cause) {
         super(logMessage, cause);
