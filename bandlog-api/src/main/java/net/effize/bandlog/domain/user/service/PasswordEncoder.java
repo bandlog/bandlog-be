8520383c342c2d56478354504a9a2fe8b79aa4ef
d0lim <limdongyoung0@gmail.com>
2025-07-15 21:49:54 +0900

    refactor: remove PasswordEncoder

--- bandlog-api/src/main/java/net/effize/bandlog/domain/user/service/PasswordEncoder.java
+++
@@ -1,8 +0,0 @@
-package net.effize.bandlog.domain.user.service;
-
-public interface PasswordEncoder {
-
-    String encode(String rawPassword);
-
-    boolean matches(String inputPassword, String hashedPassword);
-}
