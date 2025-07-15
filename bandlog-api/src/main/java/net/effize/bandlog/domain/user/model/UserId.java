211a29a34c7320d6637034bb1e538144edf64712
d0lim <limdongyoung0@gmail.com>
2025-07-15 21:58:45 +0900

    feat: add the long value getter and static factory method to the UserId

--- bandlog-api/src/main/java/net/effize/bandlog/domain/user/model/UserId.java
+++ bandlog-api/src/main/java/net/effize/bandlog/domain/user/model/UserId.java
@@ -1,5 +1,17 @@
 package net.effize.bandlog.domain.user.model;
 
 public class UserId {
-    private Long value;
+    private final Long value;
+
+    private UserId(Long value) {
+        this.value = value;
+    }
+
+    public static UserId of(Long value) {
+        return new UserId(value);
+    }
+
+    public Long longValue() {
+        return value;
+    }
 }
