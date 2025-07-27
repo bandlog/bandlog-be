d54c43707313e12dab0ff2a017e216e946ea1833
d0lim <limdongyoung0@gmail.com>
2025-07-27 17:00:30 +0900

    fix: modify JpaRepository interface generic of UserRepository

--- bandlog-api/src/main/java/net/effize/bandlog/infrastructure/user/UserRepository.java
+++ bandlog-api/src/main/java/net/effize/bandlog/infrastructure/user/UserRepository.java
@@ -2,12 +2,11 @@ package net.effize.bandlog.infrastructure.user;
 
 import net.effize.bandlog.domain.user.model.SupabaseUserId;
 import net.effize.bandlog.domain.user.model.User;
-import net.effize.bandlog.domain.user.model.UserId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, UserId> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findBySupabaseUserId(SupabaseUserId id);
 
     Boolean existsBySupabaseUserId(SupabaseUserId id);
