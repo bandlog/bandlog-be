66088904bf42a46d6c5c838d311883c61924aa9c
d0lim <limdongyoung0@gmail.com>
2025-07-27 16:48:26 +0900

    refactor: modify AuthUser fields to be vo

--- bandlog-api/src/main/java/net/effize/bandlog/application/auth/dto/AuthUser.java
+++ bandlog-api/src/main/java/net/effize/bandlog/application/auth/dto/AuthUser.java
@@ -1,14 +1,14 @@
 package net.effize.bandlog.application.auth.dto;
 
-import net.effize.bandlog.domain.user.model.User;
+import net.effize.bandlog.domain.user.model.*;
 
 public record AuthUser(
-        Long id,
-        String supabaseUserId,
-        String email,
-        String nickname
+        UserId id,
+        SupabaseUserId supabaseUserId,
+        Email email,
+        Nickname nickname
 ) {
     public AuthUser(User user) {
-        this(user.id().longValue(), user.supabaseUserId().toString(), user.email().toString(), user.nickname().toString());
+        this(user.id(), user.supabaseUserId(), user.email(), user.nickname());
     }
 }
