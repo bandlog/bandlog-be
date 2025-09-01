af1c92b5b0e65918e79a6fcc74266fe7956ccc9e
d0lim <limdongyoung0@gmail.com>
2025-09-02 01:43:06 +0900

    refactor: move JpaAuditingConfig to Config module

--- src/main/java/net/effize/bandlog/config/JpaAuditingConfig.java
+++ src/main/java/net/effize/bandlog/config/JpaAuditingConfig.java
@@ -1,4 +1,4 @@
-package net.effize.bandlog.common.config;
+package net.effize.bandlog.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
