cd1fa542b3ecab4a9cb0d260a387417676200d2b
d0lim <limdongyoung0@gmail.com>
2025-09-04 22:14:37 +0900

    refactor: update build.gradle.kts for the new module

--- bandlog-user/build.gradle.kts
+++ bandlog-user/build.gradle.kts
@@ -1,20 +1,11 @@
 plugins {
-    id("java")
-}
-
-group = "net.effize"
-version = "0.0.1-SNAPSHOT"
-
-repositories {
-    mavenCentral()
+    id("org.springframework.boot") version "3.5.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
