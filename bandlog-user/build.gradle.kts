e24353da4dd2acd4f713c68dfb728704e1e29509
d0lim <limdongyoung0@gmail.com>
2025-09-04 23:03:41 +0900

    feat: copied the EmailMalformedException into user module with the shared dependency

--- bandlog-user/build.gradle.kts
+++ bandlog-user/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
 }
 
 dependencies {
+    implementation(project(":bandlog-shared"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
