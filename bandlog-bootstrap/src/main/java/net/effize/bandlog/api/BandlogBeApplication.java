85854bc53cc6c8d3d27e2ca2ca6143b046724300
d0lim <limdongyoung0@gmail.com>
2025-09-16 23:03:46 +0900

    fix: add scanBasePackages for multi module component scan

--- bandlog-bootstrap/src/main/java/net/effize/bandlog/api/BandlogBeApplication.java
+++ bandlog-bootstrap/src/main/java/net/effize/bandlog/api/BandlogBeApplication.java
@@ -3,7 +3,7 @@ package net.effize.bandlog.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "net.effize.bandlog")
 public class BandlogBeApplication {
 
     public static void main(String[] args) {
