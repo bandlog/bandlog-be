e7857372f082473fa42de930f0752fea9b901150
d0lim <limdongyoung0@gmail.com>
2025-09-16 23:14:16 +0900

    fix: introduce a BandlogUserResponse with a consideration of module resolution

--- bandlog-adapters/bandlog-user-adapter/src/main/java/net/effize/bandlog/adapter/user/BandlogUserAdapter.java
+++ bandlog-adapters/bandlog-user-adapter/src/main/java/net/effize/bandlog/adapter/user/BandlogUserAdapter.java
@@ -1,6 +1,6 @@
 package net.effize.bandlog.adapter.user;
 
-import net.effize.bandlog.user.dto.response.UserResponse;
+import net.effize.bandlog.adapter.user.dto.BandlogUserResponse;
 import net.effize.bandlog.user.model.UserId;
 import net.effize.bandlog.user.service.UserService;
 import org.springframework.stereotype.Component;
@@ -15,7 +15,13 @@ public class BandlogUserAdapter {
         this.userService = userService;
     }
 
-    public List<UserResponse> findAllByIdIn(List<Long> ids) {
-        return userService.findAllByIdIn(ids.stream().map(UserId::new).toList());
+    public List<BandlogUserResponse> findAllByIdIn(List<Long> ids) {
+        return userService.findAllByIdIn(ids.stream().map(UserId::new).toList())
+                .stream().map(userResponse -> new BandlogUserResponse(
+                        userResponse.id(),
+                        userResponse.supabaseUserId(),
+                        userResponse.email(),
+                        userResponse.nickname()
+                )).toList();
     }
 }
