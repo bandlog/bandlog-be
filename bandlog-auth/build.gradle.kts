ca070d3383d551c471df49ddb7994a24c6660022
d0lim <limdongyoung0@gmail.com>
2025-09-18 21:52:26 +0900

    feat: add a bandlog-auth module

---
+++ bandlog-auth/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+dependencies {
+    implementation(project(":bandlog-shared"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.named<Jar>("jar") {
+    enabled = true
+}
