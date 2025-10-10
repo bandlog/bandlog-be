b651674912facae7f0a68a50778bb6532ef2fe3c
d0lim <limdongyoung0@gmail.com>
2025-10-10 18:36:47 +0900

    fix: add missing rehearsal module import for the bootstrap module

--- bandlog-bootstrap/build.gradle.kts
+++ bandlog-bootstrap/build.gradle.kts
@@ -8,6 +8,7 @@ dependencies {
     implementation(project(":bandlog-user"))
     implementation(project(":bandlog-auth"))
     implementation(project(":bandlog-team"))
+    implementation(project(":bandlog-rehearsal"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
